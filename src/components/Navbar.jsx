603a79ba52336d875e439ba3618abf594f755e55
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-09-10 14:28:22 +0100

    created navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+const Navbar = () => {
+  return <div className="flex justify-between items-center w-full h-20"></div>;
+};
+
+export default Navbar;
