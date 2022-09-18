8e81c3d3cb93e8b8cbde446a2daf367c7c5852ad
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-09-18 18:00:32 +0100

    style button in home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,18 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800 "
+      className="h-screen w-full bg-gradient-to-b from-black via-black
+       to-gray-800 "
     >
-      <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
-        <div>
-          <h2>I'm a Frontend Developer</h2>
-          <p>
+      <div
+        className=" max-w-screen-lg mx-auto flex flex-col items-center
+       justify-center h-full px-4 md:flex-row "
+      >
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text7xl font-bold text-white">
+            Hi!, I'm Esther and i'm a Frontend Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             et tortor vulputate, venenatis diam et, rutrum dui. Orci varius
             natoque penatibus et magnis dis parturient montes, nascetur
@@ -18,9 +24,13 @@ const Home = () => {
           </p>
 
           <div>
-            <button>
+            <button
+              className="text-white w-fit px-6 py-3 my-2 
+            flex items-center rounded-md bg-gradient-to-r
+             from-purple-500 to-fuchsia-500 cursor-pointer"
+            >
               Porfolio
-              <span>
+              <span className="hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight />
               </span>
             </button>
