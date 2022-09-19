6dc4cb731c4bfd9ecf1953142bd25b181cbe94f9
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-09-19 15:44:51 +0100

    style on socials completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Socials from "./components/Socials";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 function App() {
@@ -5,6 +6,8 @@ function App() {
     <div>
       <Navbar />
       <Home />
+
+      <Socials />
     </div>
   );
 }
