66952b45fc730947705785471854c2486fc00e87
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-09-21 00:17:32 +0100

    portfolio styles complete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Socials from "./components/Socials";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Experience from "./components/Experience";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
       <Socials />
     </div>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import phonie from "../assets/portfolio/phonie.png";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const portfolios = [
+    {
+      id: 1,
+      src: phonie,
+    },
+    {
+      id: 2,
+      src: phonie,
+    },
+    {
+      id: 3,
+      src: phonie,
+    },
+    {
+      id: 4,
+      src: phonie,
+    },
+    {
+      id: 5,
+      src: phonie,
+    },
+    {
+      id: 6,
+      src: phonie,
+    },
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black
+     to-gray-800 w-full text-white md:h-screen"
+    >
+      <div
+        className="max-w-screen-lg p-4 mx-auto flex
+       flex-col justify-center w-full h-full"
+      >
+        <div className="pb-8">
+          <p
+            className="text-4xl font-bold inline 
+          border-b-4 border-fuchsia-500"
+          >
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
